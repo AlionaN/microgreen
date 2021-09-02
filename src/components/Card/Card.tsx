6928f1723f1aa8f3907f0da66d76e818994659be
anovyk <anovyk@exadel.com>
2021-09-02 15:31:56 +0300

    refactor: change props declaration

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,13 @@
 import styles from './Card.module.scss';
+import Button from '../Button';
 import { ICard } from '../../shared/interfaces/card.interface';
 
 type IProps = {
   card: ICard
 }
 
-function Card(card: IProps) {
-  const { image, title, price } = card.card;
+function Card({ card }: IProps) {
+  const { image, title, price } = card;
   return (
     <li className={styles.card}>
       <img src={`${process.env.PUBLIC_URL}/assets/images/${image}`} className={styles.cardImg} alt={title} />
@@ -17,7 +18,7 @@ function Card(card: IProps) {
           $
         </div>
       </div>
-      <button type="button" className={styles.btn}>Add to cart</button>
+      <Button btnText="Add to cart" />
     </li>
   );
 }
