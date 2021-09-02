1ea99611542e25e66952fe88a001ed1be4cb7cc3
anovyk <anovyk@exadel.com>
2021-09-02 15:36:08 +0300

    feat: add link to shop in hero block

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Hero.module.scss';
 
 function Hero() {
@@ -6,6 +7,10 @@ function Hero() {
     <div className={styles.hero} style={{ backgroundImage: `url(${img})` }}>
       <div className={`${styles.heroText} ${styles.heroTextRight}`}>Micro</div>
       <div className={`${styles.heroText} ${styles.heroTextLeft}`}>Green</div>
+      <div className={styles.toShop}>
+        <div className={styles.toShopText}>Alive food for you</div>
+        <NavLink to="/shop" className={styles.btn}>To Shop</NavLink>
+      </div>
     </div>
   );
 }
