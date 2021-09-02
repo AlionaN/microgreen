2ddc92cf9e6b6bf98d111ca141e1a2a0b7b00293
anovyk <anovyk@exadel.com>
2021-09-02 15:37:02 +0300

    refactor: exact routes in another file

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,9 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-import AboutUs from './pages/AboutUs';
-import Contacts from './pages/Contacts';
-import Home from './pages/Home';
-import Payment from './pages/Payment';
-import Delivery from './pages/Delivery';
-import Shop from './pages/Shop';
-import Header from './components/Header';
+import Routes from './Routes';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/about-us">
-          <AboutUs />
-        </Route>
-        <Route path="/contacts">
-          <Contacts />
-        </Route>
-        <Route path="/payment">
-          <Payment />
-        </Route>
-        <Route path="/delivery">
-          <Delivery />
-        </Route>
-        <Route path="/shop">
-          <Shop />
-        </Route>
-      </Switch>
-    </Router>
+    <Routes />
   );
 }
 
---
+++ src/Routes.tsx
@@ -0,0 +1,42 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import AboutUs from './pages/AboutUs';
+import Contacts from './pages/Contacts';
+import Home from './pages/Home';
+import Payment from './pages/Payment';
+import Delivery from './pages/Delivery';
+import Shop from './pages/Shop';
+import Header from './components/Header';
+
+function Routes() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about-us">
+          <AboutUs />
+        </Route>
+        <Route path="/contacts">
+          <Contacts />
+        </Route>
+        <Route path="/payment">
+          <Payment />
+        </Route>
+        <Route path="/delivery">
+          <Delivery />
+        </Route>
+        <Route path="/shop">
+          <Shop />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routes;
